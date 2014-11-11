846937b7fa240e463f9b60bb9b1851f0c6a6208d
Jonas Hitomi Balslev Ishibori <20013575@post.au.dk>
2014-11-11 13:25:30 +0100

    renaming done

--- GDEMDS/DAL/ReadingCharacteristics.cs
+++ GDEMDS/DAL/ReadingCharacteristics.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    public class MeasurementCharacteristics
+    public class ReadingCharacteristics
     {
         public int Id { get; set; }
         public int SensorId { get; set; }
--- GDEMDS/DAL/SensorCharacteristics.cs
+++ GDEMDS/DAL/SensorCharacteristics.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    public class SensorModel
+    public class SensorCharacteristics
     {
         public int Id { get; set; }
         public string Description { get; set; }
