0a7f915465ed79883e758186461bf029176a8cee
Jonas Hitomi Balslev Ishibori <20013575@post.au.dk>
2014-11-11 12:52:54 +0100

    first commit - some models added

---
+++ GDEMDS/DAL/ApartmentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ApartmentModel
+    {
+        public int Id { get; set; }
+        public int floor { get; set; }
+        public int No { get; set; }
+        public double Size { get; set; }
+    }
+}
