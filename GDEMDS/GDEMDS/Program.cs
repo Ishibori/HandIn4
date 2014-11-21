d706aee693982e1eab6e8a9c7a05efb1f3d863d4
Jonas Hitomi Balslev Ishibori <20013575@post.au.dk>
2014-11-21 09:27:18 +0100

    Timer created

--- GDEMDS/GDEMDS/Program.cs
+++ GDEMDS/GDEMDS/Program.cs
@@ -4,18 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DAL;
+using DAL.Timer;
 
 namespace GDEMDS
 {
     class Program
     {
+        public static ITickTimer FLTimer { get; set; }
+        
         static void Main(string[] args)
         {
-            var jsonDown = new JsonDownloader();
-            Console.WriteLine(jsonDown.GetJson("http://userportal.iha.dk/~jrt/i4dab/E14/HandIn4/GFKRE0031_sample.txt"));
+            FLTimer = new FileLoaderTimer(5000);
+            FLTimer.TickEvent += new TickTimer(WriteToScreen);
+            FLTimer.StartTimer();
+            //var jsonDown = new JsonDownloader();
+            //Console.WriteLine(jsonDown.GetJson("http://userportal.iha.dk/~jrt/i4dab/E14/HandIn4/GFKRE0031_sample.txt"));
 
-            var JSONChar = new JSONCharacteristics();
-            JSONChar.LoadJSON();
+            //var JSONChar = new JSONCharacteristics();
+            //JSONChar.LoadJSON();
+
+            Console.ReadKey();
+        }
+
+        public static void WriteToScreen()
+        {
+            Console.WriteLine("test");
         }
     }
 }
